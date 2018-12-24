6e0047acb214f2ea35519ef7963496c4be2d9bee
Sai Charan R Patlolla <scrpatlolla@users.noreply.github.com>
2018-12-24 17:18:24 -0500

    Initial commit

---
+++ src/GridApp.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <ag-grid-vue
+      style="width:500px; height: 600px;"
+      class="ag-theme-fresh"
+      :columnDefs="columnDefs"
+      :rowData="rowData"
+    ></ag-grid-vue>
+  </div>
+</template>
+<style></style>
+
+<script type="text/javascript">
+import { AgGridVue } from "ag-grid-vue";
+import "ag-grid-enterprise";
+import { LicenseManager } from "ag-grid-enterprise";
+
+LicenseManager.setLicenseKey(
+  "Evaluation_License_Not_For_Production_Valid_Until1_March_2019__MTU1MTM5ODQwMDAwMA==571888b3c7cbc45a13d91e9c2e885c44"
+);
+
+export default {
+  props: {},
+  data() {
+    return {
+      columnDefs: [
+        { headerName: "Make", field: "make" },
+        { headerName: "Model", field: "model" },
+        { headerName: "Price", field: "price" }
+      ],
+      rowData: [
+        { make: "Toyota", model: "Celica", price: 35000 },
+        { make: "Ford", model: "Mondeo", price: 32000 },
+        { make: "Porsche", model: "Boxter", price: 72000 }
+      ]
+    };
+  },
+  computed: {},
+  watch: {},
+  created() {},
+  methods: {},
+  mounted() {},
+  beforeMount() {},
+  components: {
+    AgGridVue
+  }
+};
+</script>
---
+++ src/main.js
@@ -0,0 +1,25 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from "vue";
+import App from "./App";
+
+import GridApp from "./GridApp.vue";
+
+import { AgGridVue } from "ag-grid-vue";
+
+import "ag-grid-enterprise";
+import { LicenseManager } from "ag-grid-enterprise";
+
+LicenseManager.setLicenseKey(
+  "Evaluation_License_Not_For_Production_Valid_Until1_March_2019__MTU1MTM5ODQwMDAwMA==571888b3c7cbc45a13d91e9c2e885c44"
+);
+
+Vue.config.productionTip = false;
+
+Vue.component("grid-app", GridApp);
+
+/* eslint-disable no-new */
+new Vue({
+  el: "#app",
+  render: h => h(GridApp)
+});
